63d20c1fc34ed5cb33e3492c8f5f43f72b0c4ce3
FlyingWalross <jakob.jandl@gmail.com>
2022-03-26 04:18:19 +0100

    Added registerTeacher request Added getLoginStatus request Added checkUserNameAvailable request Updated User class to better deal with wrong usernames

--- backend/models/user.php
+++ backend/models/user.php
@@ -1,54 +1,89 @@
 <?php
 
+use LDAP\Result;
+
 require_once("models/assignment.php");
 
 class User {
-    //private $user_id;
-    //private $user_type;
+    private $user_id;
+    private $user_type;
     private $username;
-    //private $password_hash;
-    //private $first_name;
-    //private $last_name;
-    //private $user_type;
-    private $db;
+    private $password_hash;
+    private $first_name;
+    private $last_name;
     
-    public function __construct($username) {
-        $this->username = $username;
-        $this->db = new Database();
+    private $isValidUser = false;
+
+
+    //returns false if user was not found in database
+    public function initializeWithUsername($username){
+        if(!isset($username) || $username == ""){
+            return false;
+        }
+
+        $db = new Database();
+        $result = $db->getUserData($username);
+        if(!isset($result)){
+            return false;
+        }
+
+        $this->user_id = $result["pk_user_id"];
+        $this->user_type = $result["fk_user_type"];
+        $this->username = $result["username"];
+        $this->password_hash = $result["password"];
+        $this->first_name = $result["first_name"];
+        $this->last_name = $result["last_name"];
+
+        $this->isValidUser = true;
+        return true;
     }
 
     public function getFirstName(){
-        $result = $this->db->getUserData($this->username);
-        return $result["first_name"];
+        if($this->isValidUser){
+            return $this->first_name;
+        }
+        return;
     }
 
     public function getLastName(){
-        $result = $this->db->getUserData($this->username);
-        return $result["last_name"];
+        if($this->isValidUser){
+            return $this->last_name;
+        }
+        return;
+
     }
 
     public function getUsername(){
-        return $this->username;
+        if($this->isValidUser){
+            return $this->username;
+        }
+        return;
     }
 
     public function getUserId(){
-        $result = $this->db->getUserData($this->username);
-        return $result["pk_user_id"];
+        if($this->isValidUser){
+            return $this->user_id;
+        }
+        return;
     }
 
     public function getUserType(){
-        $result = $this->db->getUserData($this->username);
-        return $result["fk_user_type"];
+        if($this->isValidUser){
+            return $this->user_type;
+        }
+        return;
     }
 
-    public function getPasswordHash(){
-        $result = $this->db->getUserData($this->username);
-        return $result["password"];
+    private function getPasswordHash(){
+        if($this->isValidUser){
+            return $this->password_hash;
+        }
+        return;
     }
 
     public function matchPassword($password){
-        //hashing password
-        if($password == $this->getPasswordHash()){
+
+        if(!empty($password) && password_verify($password, $this->getPasswordHash())){
             return true;
         }
         return false;
