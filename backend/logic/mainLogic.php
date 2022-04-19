60c0233a3cc5f11605b04c717fe883a98915736a
FlyingWalross <jakob.jandl@gmail.com>
2022-04-19 22:07:39 +0200

    added createGroup added getUserGroups added getGroupName added getGroupChatId
    Co-authored-by: lo.du@gmx.at <lo.du@gmx.at>

--- backend/logic/mainLogic.php
+++ backend/logic/mainLogic.php
@@ -24,6 +24,18 @@ class MainLogic {
                 return $this->checkUserNameAvailable();
             case "registerTeacher":
                 return $this->registerTeacher();
+            case "createGroup":
+                return $this->createGroup();
+                break;
+            case "getUserGroups":
+                return $this->getUserGroups();
+                break;
+            case "getGroupName":
+                return $this->getGroupName();
+                break;
+            case "getGroupChatId":
+                return $this->getGroupChatId();
+                break;
             case "getMessages":
             
                 break;
@@ -168,5 +180,86 @@ class MainLogic {
         return $res;
     }
 
+    function createGroup(){
+        
+        if(empty($_POST["groupName"])){
+            return null;
+        }
+        
+        if(empty($_SESSION['username']) && $_SESSION['userType'] == 1){ //cecks wheter user is logged in and is teacher
+            $res["success"] = false;
+            $res["noPermission"] = true;
+            return $res;
+        }
 
+        $newGroupId = $this->db->createGroup($_POST["groupName"]);
+
+        $this->db->assignUserGroup($newGroupId, $_SESSION['userId']);
+
+        $res["success"] = true;
+        $res["newGroupId"] = $newGroupId;
+        return $res;
+    }
+
+    function getUserGroups(){
+        
+        if(empty($_SESSION['username'])){ //cecks wheter user is logged in
+            $res["success"] = false;
+            $res["notLoggedIn"] = true;
+            return $res;
+        }
+
+        $res = $this->db->getUserGroups($_SESSION['userId']);
+        
+        if(empty($res)){
+            $res["success"] = true;
+            $res["noGroups"] = true;
+            return $res;
+        }
+            return $res;
+    }
+
+    function getGroupName(){
+        if(empty($_POST["groupId"])){
+            return null;
+        }
+
+        if(empty($_SESSION['username'])){ //cecks wheter user is logged in
+            $res["success"] = false;
+            $res["notLoggedIn"] = true;
+            return $res;
+        }
+        
+        if($this->db->userIsInGroup($_SESSION['userId'], $_POST["groupId"])){
+            $res["success"] = true;
+            $res["groupName"] = $this->db->getGroupName($_POST["groupId"]);
+            return $res;
+        }
+
+        $res["success"] = false;
+        $res["userNotInGroup"] = true;
+        return $res;
+    }
+
+    function getGroupChatId(){
+        if(empty($_POST["groupId"])){
+            return null;
+        }
+
+        if(empty($_SESSION['username'])){ //cecks wheter user is logged in
+            $res["success"] = false;
+            $res["notLoggedIn"] = true;
+            return $res;
+        }
+        
+        if($this->db->userIsInGroup($_SESSION['userId'], $_POST["groupId"])){
+            $res["success"] = true;
+            $res["groupChatId"] = $this->db->getGroupChatId($_POST["groupId"]);
+            return $res;
+        }
+
+        $res["success"] = false;
+        $res["userNotInGroup"] = true;
+        return $res;
+    }
 }
