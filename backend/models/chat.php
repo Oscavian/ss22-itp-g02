7ff3cc9f56b7e995931adb00091b7ecd39282227
Oscavian <dev@oscavian.com>
2022-05-25 16:51:57 +0200

    #67 created chat & message models

--- backend/models/chat.php
+++ backend/models/chat.php
@@ -3,18 +3,44 @@
 class Chat {
     private $name;
     private $chat_id;
+    private $messages = [];
 
-    public function __construct($id = null) {
+    public function __construct(int $id = null) {
         empty(Database::select("SELECT * FROM chat WHERE pk_chat_id = ?", [$id], "i")) ? $this->chat_id = null : $this->chat_id = $id;
     }
 
-    public function getChatId() {
+    public function exists(): bool {
+        if (empty($this->chat_id)){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public function getChatId(): ?int {
         return $this->chat_id;
     }
 
-    public function getName() {
-        //TODO
+    public function getName(): string {
+        if (empty($this->name)){
+            return $this->name = Database::select("SELECT name FROM chat WHERE pk_chat_id =?", [$this->chat_id], "i", true)["name"];
+        }
         return $this->name;
     }
 
+    public function storeNewChat(string $name) {
+        $new_chat_id = Database::insert("INSERT INTO chat (name) VALUES (?)", [$name], "s");
+        if (isset($this->chat_id)){
+            $this->chat_id = $new_chat_id;
+            $this->getName();
+            $this->getMessages();
+        } else {
+            $this->chat_id = $new_chat_id;
+        }
+    }
+
+    public function getMessages(): array {
+        //TODO
+        return $this->messages;
+    }
 }
--- backend/models/message.php
+++ backend/models/message.php
@@ -1,5 +1,76 @@
 <?php
 
 class Message {
+    private $message_id;
+    private $user_id;
+    private $chat_id;
+    private $text;
+    private $time;
+    private $file_path;
+    private $image_path;
 
+
+    public function __construct($id = null) {
+        empty(Database::select("SELECT * FROM message WHERE pk_message_id = ?", [$id], "i")) ? $this->message_id = null : $this->message_id = $id;
+
+    }
+
+    public function getBaseData() {
+        $query = "SELECT pk_message_id as message_id, fk_user_id as user_id, fk_chat_id as chat_id, time, text FROM message WHERE pk_message_id = ?";
+        $result = Database::select($query, [$this->message_id], "i");
+
+        $this->user_id = $result["user_id"];
+        $this->text = $result["text"];
+        $this->chat_id = $result["chat_id"];
+        $this->time = $result["time"];
+
+        return get_object_vars($this);
+    }
+
+    public function storeNewMessage($user_id, $chat_id, $text){
+
+        $new_message_id = Database::insert("INSERT INTO message (fk_user_id, fk_chat_id, text) values (?, ?, ?)", [$user_id, $chat_id, $text], "iis");
+        if (isset($this->message_id)){
+            $this->message_id = $new_message_id;
+            $this->getBaseData();
+        } else {
+            $this->message_id = $new_message_id;
+        }
+    }
+
+    public function getMessageId() {
+        return $this->message_id;
+    }
+
+
+    public function getUserId() {
+        if (empty($this->user_id)){
+            return $this->user_id = Database::select("SELECT fk_user_id AS user_id FROM message WHERE pk_message_id = ?")["user_id"];
+        }
+        return $this->user_id;
+    }
+
+
+    public function getText() {
+        if (empty($this->text)){
+            return $this->text = Database::select("SELECT text FROM message WHERE pk_message_id = ?")["text"];
+        }
+        return $this->text;
+    }
+
+
+    public function getChatId() {
+        if (empty($this->chat_id)){
+            return $this->chat_id = Database::select("SELECT fk_chat_id AS chat_id FROM message WHERE pk_message_id = ?")["chat_id"];
+        }
+        return $this->chat_id;
+    }
+
+
+    public function getTime() {
+        if (empty($this->time)){
+            return $this->time = Database::select("SELECT time AS user_id FROM message WHERE pk_message_id = ?")["time"];
+        }
+        return $this->time;
+    }
 }
