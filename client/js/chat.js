3d591e6350287623b68e2c5e62d8f7da0c23b021
FlyingWalross <jakob.jandl@gmail.com>
2022-06-05 15:24:13 +0200

    finished chat logic

--- client/js/chat.js
+++ client/js/chat.js
@@ -1,69 +1,193 @@
-let counter = 0; // unnötig wenn ids von der DB genommen werden
-loadMessages();
-function loadMessages(){
-    /*$.ajax({
+var groupId = new URLSearchParams(window.location.search).get("id");
+loadfirstMessages();
+
+var messageLoadOffset = 0;
+var currentDate = new Date();
+
+function loadfirstMessages(){
+
+    $.ajax({
         type: "POST",
         url: "/ss22-itp-g02/backend/requestHandler.php",
-        data: {method: "getMessages", chatId_groupId: hi},
+        data: {method: "getMessages", group_id: groupId, offset: 0},
         cache: false,
         dataType: "json",
         success: (response) => {
-            let counter = 0; wenn nur eine gewisse Anzahl Nachrichten geladen werden soll
-            $.each(response =>{
-                while(counter < 25){
-                    Nachricht einfügen
-                    counter++;
+
+            if(!response.length){ //prevents scroll eventlistener from being set when there are no more messages to load
+                return;
+            }
+
+            currentDate = new Date(response[0]["time"]);
+            $.each(response, (index, message) => {
+
+                messageDate = new Date(message["time"])
+
+                if(messageDate.getDate() != currentDate.getDate() || ((messageDate - currentDate) / 86400000) > 2){
+                    insertDateTag(currentDate);
+                    currentDate = messageDate;
+                }
+                
+                insertMessage(message);
+            });
+
+            chatContent = $("#chatContent")
+            chatContent.animate({ scrollTop: chatContent[0].scrollHeight }, 0);
+        
+            chatContent.off("scroll");
+            chatContent.on("scroll", function() {
+                var pos = chatContent.scrollTop();
+                if (pos < 200) {
+                    chatContent.off("scroll");
+                    loadMoreMessages();
                 }
             });
+
+        },
+        error: function(error){
+            console.log("AJAX-Request error: " + error);
+            alert("Error loading messages!");
         }
-    });*/
-    ///////////////////// folgende Zahlen sind nur Platzhalter für die messageId aus der DB, Vorname + Nachname + Uhrzeit ebenfalls
-    while(counter < 5){
-        $("#textsField").append("<div class='card chatMsg msgOwn' id='msg" + counter + "'>" +
-        "<div class='senderInfo'>Vorname Nachname - Uhrzeit</div>" +
-        "<div class='card-text'>" +
-        "eigene Chatnachricht ffffffffffffffffff fffffffffff fffffffffff ffffffffff ffffffffffffffffffffffff fffffffffff" +
-        "</div></div>");
-        if(isTeacher === true){
-            $("#msg" + counter + "").append("<button class='btn btn-sm py-0 px-1 btnDeleteMsg' onclick='deleteMessage(" + counter + ")'><i class='bi bi-trash3'></i></button>");
-        }
-        counter++;
-    }
-    
-    $('#chatContent').animate({scrollTop: document.body.scrollHeight},"fast");
+    });
 }
 
-function sendMessage(){
-    let message = $("#newChatMessage").val();
-    $("#newChatMessage").val("");
-    console.log(message);
-    /*$.ajax({
+function loadMoreMessages(){
+
+    messageLoadOffset++;
+
+    $.ajax({
         type: "POST",
         url: "/ss22-itp-g02/backend/requestHandler.php",
-        data: {method: "sendMessage", message: message},
+        data: {method: "getMessages", group_id: groupId, offset: messageLoadOffset},
         cache: false,
         dataType: "json",
-        success: (response) => {}
-    });*/
-    $("#textsField").append("<div class='card chatMsg msgOwn' id='msg" + counter + "'>" +
-    "<div class='senderInfo'>Vorname Nachname - Uhrzeit</div>" +
-    "<div class='card-text'>" + message +
-    "</div></div>");
-    if(isTeacher === true){
-        $("#msg" + counter + "").append("<button class='btn btn-sm py-0 px-1 btnDeleteMsg' onclick='deleteMessage(" + counter + ")'><i class='bi bi-trash3'></i></button>");
+        success: (response) => {
+
+            if(!response.length){ //prevents scroll eventlistener from being set when there are no more messages to load
+                insertDateTag(currentDate);
+                return;
+            }
+
+            $.each(response, (index, message) => {
+
+                messageDate = new Date(message["time"])
+
+                if(messageDate.getDate() != currentDate.getDate() || ((messageDate - currentDate) / 86400000) > 2){
+                    insertDateTag(currentDate);
+                    currentDate = messageDate;
+                }
+                
+                insertMessage(message);
+            });
+        
+            chatContent.off("scroll");
+            chatContent.on("scroll", function() {
+                var pos = chatContent.scrollTop();
+                if (pos < 200) {
+                    chatContent.off("scroll");
+                    loadMoreMessages();
+                }
+            });
+        },
+        error: function(error){
+            console.log("AJAX-Request error: " + error);
+            alert("Error loading messages!");
+        }
+    });
+}
+
+function insertDateTag(date) {
+    $("#textsField").prepend(`
+    <div class="chatDate">
+        <span>${String(date.getDate()).padStart(2, "0")}.${String(date.getMonth() + 1).padStart(2, "0")}</span>
+    </div>`);
+}
+
+function insertMessage(message) {
+
+    insertMessageDate = new Date(message["time"])
+    insertMessageDateHours = new String(insertMessageDate.getHours()).padStart(2, "0");
+    insertMessageDateMinutes = new String(insertMessageDate.getMinutes()).padStart(2, "0");
+    insertMessageTimeString = insertMessageDateHours + ":" + insertMessageDateMinutes;
+
+    $("#textsField").prepend(`
+    <div id='message${message["pk_message_id"]}' class="${message["isOwnMessage"] ? "ownMessageFlex" : "otherMessageFlex"}">
+        <div class='card chatMsg ${message["isOwnMessage"] ? "msgOwn" : "msgOther"}'>
+            <div class='senderInfo'>${!message["isOwnMessage"] ? message["first_name"] + " " + message["last_name"] : "Du"} - ${insertMessageTimeString}</div>
+            <div class='card-text'>${message["text"]}</div>
+        </div>
+        ${isTeacher ? "<button class='btn btn-sm py-0 px-1 btnDeleteMsg' onclick='deleteMessage(" + message["pk_message_id"] + ")'><i class='bi bi-trash3'></i></button>" : ''}
+    </div>`);
+}
+
+function insertNewSentMessage(message) {
+    
+    insertMessageDate = new Date(message["time"])
+    insertMessageDateHours = new String(insertMessageDate.getHours()).padStart(2, "0");
+    insertMessageDateMinutes = new String(insertMessageDate.getMinutes()).padStart(2, "0");
+    insertMessageTimeString = insertMessageDateHours + ":" + insertMessageDateMinutes;
+
+    $("#textsField").append(`
+    <div id='message${message["pk_message_id"]}' class="ownMessageFlex">
+        <div class='card chatMsg msgOwn'>
+            <div class='senderInfo'>Du - ${insertMessageTimeString}</div>
+            <div class='card-text'>${message["text"]}</div>
+        </div>
+        ${isTeacher ? "<button class='btn btn-sm py-0 px-1 btnDeleteMsg' onclick='deleteMessage(" + message["pk_message_id"] + ")'><i class='bi bi-trash3'></i></button>" : ''}
+    </div>`);
+}
+
+function sendMessage(e){
+    e.preventDefault();
+    let messageText = $("#newChatMessage").val();
+
+    if(messageText == ""){
+        return;
     }
-    counter++;
-    $('#chatContent').animate({scrollTop: document.body.scrollHeight},"fast");
+
+    $("#newChatMessage").val("");
+
+    $.ajax({
+        type: "POST",
+        url: "/ss22-itp-g02/backend/requestHandler.php",
+        data: {method: "sendMessage", text: messageText, group_id: groupId},
+        cache: false,
+        dataType: "json",
+        success: (response) => {
+            if(response["success"]){
+                message = {
+                    "time": new Date().getTime(),
+                    "text" : messageText,
+                    "pk_message_id": response["message_id"]
+                };
+                insertNewSentMessage(message);
+            }
+        },
+        error: function(error){
+            console.log("AJAX-Request error: " + error);
+            alert("Error sending message!");
+        }
+    });
+
+    chatContent = $("#chatContent")
+    chatContent.animate({ scrollTop: chatContent[0].scrollHeight }, "fast");
 }
 
 function deleteMessage(msgId){
-    console.log(msgId);
-    /*$.ajax({
+    $.ajax({
         type: "POST",
         url: "/ss22-itp-g02/backend/requestHandler.php",
-        data: {method: "deleteMessage", messageId: msgId},
+        data: {method: "deleteMessage", message_id: msgId},
         cache: false,
         dataType: "json",
-        success: (response) => {}
-    });*/
+        success: (response) => {
+            if(response["success"]){
+                $("#message" + msgId).remove();
+            }
+        },
+        error: function(error){
+            console.log("AJAX-Request error: " + error);
+            alert("Error deleting Message!");
+        }
+    });
 }
